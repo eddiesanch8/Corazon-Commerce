fb7a7981f6cab8d4894d080f12a6dae603e5911e
Eduardo <46777616+eddiesanch8@users.noreply.github.com>
2025-08-06 15:39:12 -0400

    added css and html for nav

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log(`connected!`);
+// ----------------------------------- NavBar DOM -------------------------------------\\
+const navBar = document.querySelector("[data-nav-bar]");
+
+navBar.addEventListener("click", () => {
+  console.log("hi");
+});
