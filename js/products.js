10ddb8787e0a3a3e1baf16878958ffbd70c1cf66
Eduardo <46777616+eddiesanch8@users.noreply.github.com>
2025-08-12 12:18:02 -0400

    finsihed homepage CSS

--- js/products.js
+++ js/products.js
@@ -1,30 +1,34 @@
 "use strict";
 
-fetch("../products.json") // Specify the path to your JSON file
+// path to JSON
+fetch("../products.json")
+  // Error handling in case you don't get the .json file
   .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    return response.json(); // Parse the response as JSON
+    // Parse the response as JSON
+    return response.json();
   })
+  // Now, with the response, we use an array method since my JSON stores my product details as an array of objects
   .then((products) => {
     products.products.forEach((product) => {
       createProduct(product);
     });
-
-    // createProduct(products.products[0]);
-    // console.log(products.products[0]);
   })
   .catch((error) => {
-    console.error("Failed to fetch data:", error); // Handle any errors
+    // Error handling
+    console.error("Failed to fetch data:", error);
   });
 
+// function that takes in every object from JSON
 function createProduct(obj) {
-  const newDiv = document.createElement("section");
+  // Creates new section and gets Parent Grid
+  const newSection = document.createElement("section");
   const parentGrid = document.querySelector("[data-grid]");
-
-  newDiv.classList.add("product-grid");
-
+  // adds css styling
+  newSection.classList.add("product-grid");
+  // Inner html for every section
   let productCard = `
         <img class="product-img" src="${obj.image}" alt="${obj.alt}"/>
         <p class="product-name">${obj.name}</p>
@@ -34,6 +38,7 @@ function createProduct(obj) {
         </p>
         <button class="product-btn">Add to Cart</button>
         `;
-  newDiv.innerHTML = productCard;
-  parentGrid.append(newDiv);
+  newSection.innerHTML = productCard;
+  // add to parent grid
+  parentGrid.append(newSection);
 }
